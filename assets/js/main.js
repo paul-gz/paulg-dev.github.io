a9d5c7d69c98a1d5e443d879a9e3c13443313ffb
John Paul Santos <santos.johnpaul.code@gmail.com>
2023-02-20 18:33:54 +0800

    Update to bootstrap 5.3, fix whitespace overflow

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 (function() {
   "use strict";
 
-  /* Easy selector helper function */
+  /*Easy selector helper function*/
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
